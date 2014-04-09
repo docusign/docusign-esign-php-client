71df241202a945d3426f7dbd7e183cbb9113d4a0
Erin Marchak <erin@myplanetdigital.com>
2014-04-09 09:59:03 -0400

    added recipinet tabs into DocuSign_Recipient object

--- src/service/DocuSign_RequestSignatureService.php
+++ src/service/DocuSign_RequestSignatureService.php
@@ -113,6 +113,7 @@ class DocuSign_RequestSignatureResource extends DocuSign_Resource {
 					"name" => $recipient->getName(),
 					"email" => $recipient->getEmail(),
 					"clientUserId" => $recipient->getClientId(),
+					"tabs" => $recipient->getTabs(),
 				);
 			}
 			$data['recipients'] = $recipientsList;
@@ -164,8 +165,9 @@ class DocuSign_Recipient extends DocuSign_Model {
 	private $email;
 	private $clientId;
 	private $type;
+	private $tabs;
 
-	public function __construct($routingOrder, $id, $name, $email, $clientId = NULL, $type = 'signers') {
+	public function __construct($routingOrder, $id, $name, $email, $clientId = NULL, $type = 'signers', $tabs = NULL) {
 		if( isset($routingOrder) ) $this->routingOrder = $routingOrder;
 		if( isset($id) ) $this->id = $id;
 		if( isset($name) ) $this->name = $name;
@@ -183,14 +185,21 @@ class DocuSign_Recipient extends DocuSign_Model {
 					break;
 			}
 		}
-
+		if( isset($tabs) && is_array($tabs)) {
+			foreach ($tabs as $tabType => $tab) {
+				foreach ($tab as $singleTab) {
+					$this->setTab($tabType, $singleTab);
+				}
+			}
+		}
 	}
 
-  	public function setRoutingOrder($routingOrder) { $this->routingOrder = $routingOrder; }
+
+	public function setRoutingOrder($routingOrder) { $this->routingOrder = $routingOrder; }
 	public function getRoutingOrder() { return $this->routingOrder; }
 	public function setId($id) { $this->id = $id; }
 	public function getId() { return $this->id; }
-  	public function setName($name) { $this->name = $name; }
+	public function setName($name) { $this->name = $name; }
 	public function getName() { return $this->name; }
 	public function setEmail($email) { $this->email = $email; }
 	public function getEmail() { return $this->email; }
@@ -198,6 +207,57 @@ class DocuSign_Recipient extends DocuSign_Model {
 	public function getClientId() { return $this->clientId; }
 	public function setType($type) { $this->type = $type; }
 	public function getType() { return $this->type; }
+	public function getTabs() { return $this->tabs; }
+	public function getTab($tabType, $tabLabel) {
+		foreach ($this->tabs[$tabType] as $tab) {
+			if ($tab['tabLabel'] == $tabLabel) {
+				return array($tabType => $tab);
+			}
+		}
+	}
+
+	public function setTab($tabType, $tab)
+	{
+		//.. construct tab array
+		switch ($tabType) {
+			case 'textTabs':
+			case 'approveTabs':
+			case 'checkboxTab':
+			case 'companyTabs':
+			case 'dateSignedTabs':
+			case 'dateTabs':
+			case 'declineTabs':
+			case 'emailTabs':
+			case 'emailAddressTabs':
+			case 'envelopeIdTabs':
+			case 'firstNameTabs':
+			case 'formulaTabs':
+			case 'fullNameTabs':
+			case 'initialHereTabs':
+			case 'lastNameTabs':
+			case 'noteTabs':
+			case 'listTabs':
+			case 'numberTabs':
+			case 'radioGroupTabs':
+			case 'signHereTabs':
+			case 'signerAttachmentTabs':
+			case 'ssnTabs':
+			case 'textTabs':
+			case 'titleTabs':
+			case 'zipTabs':
+				$this->tabs[$tabType][] = $tab;
+				break;
+		};
+	}
+
+	public function unsetTab($tabType, $tabLabel)
+	{
+		foreach ($this->tabs[$tabType] as &$tab) {
+			if ($tab['tabLabel'] == $tabLabel) {
+				unset($tab);
+			}
+		}
+	}
 }
 
 
