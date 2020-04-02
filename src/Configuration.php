aa957b0a59bf41b8bd72cc19add773a5026bba0e
Dhaval Brahmbhatt <32913565+dbbrahmbhatt@users.noreply.github.com>
2020-04-02 11:44:37 -0700

    Version 5.1.0-v2.1-20.1.00 (#94)
    * Version 5.1.0-v2.1-20.1.00
    
    * corrected date
    
    Co-authored-by: Dhaval Brahmbhatt <dhaval.brahmbhatt@dsa017178.docusignhq.com>

--- src/Configuration.php
+++ src/Configuration.php
@@ -109,7 +109,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = "Swagger-Codegen/5.1.0-rc/php";
+    protected $userAgent = "Swagger-Codegen/5.1.0/php";
 
     /**
      * Debug switch (default set to false)
@@ -695,7 +695,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . phpversion() . PHP_EOL;
         $report .= '    OpenAPI Spec Version: v2.1' . PHP_EOL;
-        $report .= '    SDK Package Version: 5.1.0-rc' . PHP_EOL;
+        $report .= '    SDK Package Version: 5.1.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
